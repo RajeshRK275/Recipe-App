e86ad35c0dda8525337282e83dce8bdc4cef2bc1
Rajeshkumar_27 <rajesh@leaguex.com>
2024-09-24 06:57:45 +0530

    searchBar

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import styles from "../Styles/search.module.css"
 export default function Search(){
+    const[query, setQuery] = useState("pizza");
     return(
         <div className={styles.searchBar}>
-            <input type="text"/>
+            <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
             <button type="submit">Search</button>
         </div>
     );
