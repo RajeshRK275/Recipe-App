17ca2501bccf5a3eeb7860682e076ddc6f668387
Rajeshkumar_27 <rajesh@leaguex.com>
2024-09-25 06:54:03 +0530

    Data fetched from food api

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-import styles from "../Styles/search.module.css"
-export default function Search(){
-    const[query, setQuery] = useState("pizza");
-    return(
-        <div className={styles.searchBar}>
-            <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
-            <button type="submit">Search</button>
-        </div>
-    );
+import { useEffect, useState } from "react";
+import styles from "../Styles/search.module.css";
+
+export default function Search() {
+  const [query, setQuery] = useState("pizza");
+
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}?query=${query}&apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      const data = await res.json();
+      console.log(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div className={styles.searchBar}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </div>
+  );
 }
